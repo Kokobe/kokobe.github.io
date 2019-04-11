d35057b0c67b3d7191194cf858e45991e74a0390
Kokobe <KobeChang@dhcp-10-8-039-147.mobile.reshsg.uci.edu>
2019-04-11 02:27:31 -0700

    mobile parallax

--- parallax.js
+++ parallax.js
@@ -6,7 +6,7 @@
 		windowHeight = $window.height();
 	});
 
-	$.fn.parallax = function(xpos, speedFactor, outerHeight) {
+	$.fn.parallax = function(xpos, speedFactor, outerHeight, widthLimit) {
 		var $this = $(this);
 		var getHeight;
 		var firstTop;
@@ -31,23 +31,28 @@
 		if (arguments.length < 1 || xpos === null) xpos = "50%";
 		if (arguments.length < 2 || speedFactor === null) speedFactor = 0.1;
 		if (arguments.length < 3 || outerHeight === null) outerHeight = true;
+		if (arguments.length < 4 || widthLimit === null) widthLimit = null;
 		
 		// function to be called whenever the window is scrolled or resized
 		function update(){
 			var pos = $window.scrollTop();				
 
-			$this.each(function(){
-				var $element = $(this);
-				var top = $element.offset().top;
-				var height = getHeight($element);
+			if ($window.width() > widthLimit || widthLimit == null){
+				$this.each(function(){
+					var $element = $(this);
+					var top = $element.offset().top;
+					var height = getHeight($element);
+	
+					// Check if totally above or totally below viewport
+					if (top + height < pos || top > pos + windowHeight) {
+						return;
+					}
+	
+					$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
+				});
 
-				// Check if totally above or totally below viewport
-				if (top + height < pos || top > pos + windowHeight) {
-					return;
-				}
-
-				$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
-			});
+			}
+			
 		}		
 
 		$window.bind('scroll', update).resize(update);
