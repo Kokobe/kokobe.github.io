83fcecbd75857a7a609fac6c52aa075bfae941e2
Kokobe <KobeChang@dhcp-10-8-039-147.mobile.reshsg.uci.edu>
2019-04-11 20:14:09 -0700

    test5

--- parallax.js
+++ parallax.js
@@ -68,7 +68,7 @@
 						return;
 					}
 	
-					$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * .1) + "px");
+					$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * .01) + "px");
 				});
 			}
 			
